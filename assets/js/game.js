6a65511bd9cfea2e5680965579a6eec3022e46a0
Phillip Rognerud <philliprognerud2@yahoo.com>
2017-06-17 23:05:36 -0700

    Refactored code, made it more simple and reliable

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,183 +1,91 @@
 var GAME_SIZE = 30;
+var NUM_COLUMNS = 5;
+var SQUARE_DIMENSION = 100;
+var MARGINS = 1.5;
 var GAME_WIDTH = 515;
+
 var DEFAULT_COLOR = "#1DB01D";
 var USER_COLOR = "#168484";
 var OPPONENT_COLOR = "#EB4E4E";
 
+var myScore = 0;
+var aiScore = 0;
+
 init();
 
 function init(){
-  setWidth();
-  setHeight();
   createBoard();
-  setupNumbers();
+  addGameListeners();
+}
+
+function addGameListeners(){
+  var arr = $(".number").on("mouseenter mouseleave click", function(event){
+    if(event.type === "mouseenter"){
+      if(!$(this).hasClass("clicked")){
+        $(this).css({"background": USER_COLOR});
+        highlightFactors(arr, $(this), false);
+      }
+
+    } else if(event.type === "mouseleave"){
+      if(!$(this).hasClass("clicked")){
+        $(this).css({"background": DEFAULT_COLOR});
+        unhighlightFactors(arr, $(this));
+      }
+
+    } else if(event.type === "click"){
+      if(!$(this).hasClass("clicked")){
+        $(this).css({"background": USER_COLOR}).addClass("clicked").off('mouseenter mouseleave click');
+        highlightFactors(arr, $(this), true);
+      }
+    }
+  });
+}
+
+function unhighlightFactors(arr, number){
+  var n = parseInt(number.text());
+  for(var i = 0; i < arr.length-1; i++){
+    if(n % parseInt($(arr[i]).text()) == 0) {
+      if(!$(arr[i]).hasClass("clicked")){
+        $(arr[i]).css({"background": DEFAULT_COLOR});
+      }
+    }
+  }
+}
+
+function highlightFactors(arr, number, clicked){
+  var n = parseInt(number.text());
+  for(var i = 0; i < n-1; i++){
+    if(n % parseInt($(arr[i]).text()) == 0){
+      if(!$(arr[i]).hasClass("clicked") && clicked){
+        $(arr[i]).css({"background": OPPONENT_COLOR}).addClass("clicked").off('mouseenter mouseleave click');
+      } else {
+        $(arr[i]).css({"background": OPPONENT_COLOR});
+      }
+    }
+  }
 }
 
 function createBoard(){
   for(var i = 0; i < GAME_SIZE; i++){
     $(".game").append("<div class='number fadein'>" + (i+1) + "</div>");
+    $(".number").css({"background": DEFAULT_COLOR});
   }
+  setHeight();
+  setWidth();
 }
 
-function setupNumbers(){
-  var numbers = [];
 
-  $(".number").each(function(key, value){
-    numbers.push(value);
-    $(value).mouseenter(function(){
-      focusFactors($(this), numbers, false);
-    });
-
-    $(value).click(function(){
-      if(!$(this).hasClass("clicked")){
-        $(this).css("background", USER_COLOR);
-        $(this).addClass("clicked");
-
-        calculateFactors(parseInt($(this).text()), numbers, false);
-
-        if(!$(this).hasClass("scoreAdded")){
-          var currentScore = parseInt($("#mypt").text());
-          var numberToAdd = parseInt($(this).text())
-          $("#mypt").text(currentScore + numberToAdd);
-          opponentTurn(numbers);
-        }
-      }
-    });
-
-    $(value).mouseleave(function(){
-      unfocusFactors($(this), numbers);
-    });
-  });
-
-}
-
-function calculateFactors(number, arr, opponent){
-
-  for(var i = 0; i < number-1; i++){
-    var temp = parseInt($(arr[i]).text());
-    if(number%temp == 0 && !opponent && !$(arr[i]).hasClass("oponnent2")){
-      addToScore(arr, true, i);
-      $(arr[i]).addClass("clicked");
-      $(arr[i]).addClass("opponent2");
-    } else if (number%temp == 0 && opponent && !$(arr[i]).hasClass("opponent")) {
-      addToScore(arr, false, i);
-      $(arr[i]).addClass("clicked");
-      $(arr[i]).addClass("opponent");
-    }
-  }
-}
-
-function opponentTurn(arr){
-  var newArr = arr.map(function(item){
-    if(!$(item).hasClass("clicked")){
-      return $(item).text();
-    }
-  }).reverse();
-
-  console.log(arr);
-  var factorsAdded = 0;
-  var bestChoice = 0;
-  var bestIndex = 0;
-  newArr.forEach(function(number, index){
-    for(var x = 0; x < newArr.length; x++){
-      var intNum1 = parseInt(number);
-      var intNum2 = parseInt(newArr[x]);
-      var netDifference = 0;
-
-      if(intNum1 % intNum2 == 0 && intNum2 !== intNum1){
-        factorsAdded += intNum2;
-      }
-
-      if(x === newArr.length-1){
-        netDifference = intNum1 - factorsAdded;
-
-        if(netDifference > bestChoice){
-          bestChoice = netDifference;
-          bestIndex = index;
-        }
-      }
-    }
-
-    factorsAdded = 0;
-    netDifference = 0;
-  });
-
-  opponentMove(arr, arr.length-bestIndex-1);
-}
-
-function opponentMove(arr, index){
-    setTimeout(function(){
-      focusFactors($(arr[index]), arr, true);
-      calculateFactors(parseInt($(arr[index]).text()), arr, true);
-
-      addToScore(arr, true, index);
-
-    }, 2000);
-}
-
-function addToScore(arr, opponent, index){
-  if(!$(arr[index]).hasClass("opponent") && opponent && !$(arr[index]).hasClass("clicked")){
-    var currentScore = parseInt($("#aipt").text());
-    var numberToAdd = parseInt($(arr[index]).text());
-    $("#aipt").text(currentScore + numberToAdd);
-  } else if (!$(arr[index]).hasClass("opponent2") && !opponent  && !$(arr[index]).hasClass("clicked")) {
-    var currentScore = parseInt($("#mypt").text());
-    var numberToAdd = parseInt($(arr[index]).text());
-    $("#mypt").text(currentScore + numberToAdd);
-  }
-}
-
-function focusFactors(value, arr, opponent){
-  setTimeout(function(){
-    //calcualte the factors and hihglight opponent color
-    var number = parseInt(value.text());
-
-    if(!opponent && !$(value).hasClass("opponent") && !$(value).hasClass("opponent2")){
-      $(value).css("background", USER_COLOR);
-    } else {
-      $(value).css("background", OPPONENT_COLOR);
-    }
-
-    for(var i = 0; i < number-1; i++){
-      var temp = parseInt($(arr[i]).text());
-      if(number%temp == 0 && !$(arr[i]).hasClass("clicked") && !opponent){
-        $(arr[i]).css("background", OPPONENT_COLOR);
-      } else if (number%temp == 0 && $(arr[i]).hasClass("opponent") && opponent) {
-        $(arr[i]).css("background", USER_COLOR);
-      }
-    }
-  }, 150);
-
-  return arr;
-}
-
-function unfocusFactors(value, arr){
-  //calcualte the factors and hihglight opponent color
-  setTimeout(function(){
-    var number = parseInt(value.text());
-    var clicked = $(value).hasClass("clicked");
-
-    if(!clicked){
-      $(value).css("background", DEFAULT_COLOR);
-    }
-
-    for(var i = 1; i < number; i++){
-      if(number%i == 0 && !clicked && !$(arr[i-1]).hasClass("clicked")){
-        $(arr[i-1]).css("background", DEFAULT_COLOR);
-      } else if (number%i == 0 && clicked){
-        $(arr[i-1]).addClass("clicked");
-      }
-    }
-  }, 150);
-
-  return arr;
-}
 
 function setWidth(){
   $(".game").width(GAME_WIDTH);
   $(".score").width(GAME_WIDTH);
+  $(".number").css({
+    "width": SQUARE_DIMENSION,
+    "height": SQUARE_DIMENSION,
+    "margin": MARGINS
+  });
 }
 
 function setHeight(){
-  $(".game").height(100 * (GAME_SIZE/5) + ((GAME_SIZE)/5)*3);
+  $(".game").height(SQUARE_DIMENSION * (GAME_SIZE/NUM_COLUMNS) + (GAME_SIZE/NUM_COLUMNS) * (MARGINS*2));
 }
